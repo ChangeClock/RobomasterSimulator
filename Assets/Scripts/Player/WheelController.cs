bca843ef40e8ba1de89b0133e1441acd080e4de5
chuan <11811426@mail.sustech.edu.cn>
2023-06-18 01:46:40 +0800

    fix: fix wheel ground detection issue

--- Assets/Scripts/Player/WheelController.cs
+++ Assets/Scripts/Player/WheelController.cs
@@ -9,20 +9,30 @@ public class WheelController : MonoBehaviour
     // A boolean property to store whether the gameobject is currently colliding with anything.
     private bool isColliding = false;
 
-    // OnTriggerEnter is called when this gameobject first enters a collision with another object.
-    private void OnCollisionEnter(Collision collision)
+    // // OnTriggerEnter is called when this gameobject first enters a collision with another object.
+    // private void OnCollisionEnter(Collision collision)
+    // {
+    //     if (collision.gameObject.tag == "Ground") 
+    //     {
+    //         isColliding = true;
+    //     }
+    // }
+
+    // // OnTriggerExit is called when this gameobject exits a collision with another object.
+    // private void OnCollisionExit(Collision collision)
+    // {
+    //     if (collision.gameObject.tag == "Ground") 
+    //     {
+    //         isColliding = false;
+    //     }
+    // }
+
+    private void OnCollisionStay(Collision collision) 
     {
         if (collision.gameObject.tag == "Ground") 
         {
             isColliding = true;
-        }
-    }
-
-    // OnTriggerExit is called when this gameobject exits a collision with another object.
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.tag == "Ground") 
-        {
+        } else {
             isColliding = false;
         }
     }

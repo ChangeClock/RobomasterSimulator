9a8035d86f272412b6e45306225fbd52bcf6703c
chuan <11811426@mail.sustech.edu.cn>
2023-11-16 00:38:20 +0800

    perf: perfect script files structure

--- Assets/Scripts/Game/BasicComponents/WheelController.cs
+++ Assets/Scripts/Game/BasicComponents/WheelController.cs
@@ -35,7 +35,7 @@ public class WheelController : MonoBehaviour
     {
         if (collision.gameObject.tag == "Ground") 
         {
-            Debug.Log("Exit");
+            // Debug.Log("Exit");
             isCollidingCounterLock = false;
         }
     }
